c4962a999a6aad0b5551ac5cb0525c86908d7ccd
Sathuzan <N.Sathuzan@gmail.com>
2024-01-13 00:39:01 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:url_launcher/url_launcher.dart';
 //import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,48 +16,50 @@ class MyApp extends StatelessWidget {
       title: 'Contact Card',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        textTheme: TextTheme(
+        textTheme: const TextTheme(
           titleLarge: TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
-      home: ContactCard(),
+      home: const ContactCard(),
     );
   }
 }
 
 class ContactCard extends StatelessWidget {
+  const ContactCard({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('Contact Card')),
+        title: const Center(child: Text('Contact Card')),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            CircleAvatar(
+            const CircleAvatar(
               backgroundImage: AssetImage('android/Image/Profile.jpg'),
               radius: 50,
             ),
-            SizedBox(height: 16),
-            Text(
+            const SizedBox(height: 16),
+            const Text(
               'Narendiran Sathuzan',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 8),
-            Text('Student'),
-            SizedBox(height: 16),
-            Text('+94-77-018-45-14'),
-            SizedBox(height: 8),
-            Text('n.sathuzan@gmail.com'),
-            SizedBox(height: 16),
+            const SizedBox(height: 8),
+            const Text('Student'),
+            const SizedBox(height: 16),
+            const Text('+94-77-018-45-14'),
+            const SizedBox(height: 8),
+            const Text('n.sathuzan@gmail.com'),
+            const SizedBox(height: 16),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 IconButton(
-                  icon: SizedBox(
+                  icon: const SizedBox(
                     height: 30.0,
                     width: 30.0,
                     child: Image(
@@ -73,7 +77,7 @@ class ContactCard extends StatelessWidget {
                   //color: Colors.blue,
                 ),
                 IconButton(
-                  icon: SizedBox(
+                  icon: const SizedBox(
                     height: 30.0,
                     child: Image(
                       image: AssetImage('android/Image/github.png'),
